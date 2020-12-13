0189e730ad1cd69f26987fc43f7cf16f0f7959f7
fimal <fimal@radware.com>
2020-12-13 10:22:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Free5gc Stage3 for Kubernetes
+The free5GC is an open-source project for 5th generation (5G) mobile core networks. The ultimate goal of this project is to implement the 3GPP Release 15 (R15) and Release 16 (R16) 5G core network (5GC).
+
+This repository aims to install dockerized free5gc stage3 and ride on kubernetes.
+
+NOTE: Can work at version `5.0.0-23-generic` to run UPF(GTP5G Module.)
---
+++ manifests/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# - the right sequence to start mongo upf nrf amf smf udr pcf udm nssf ausf webui
+NF_LIST="f5gc-upf f5gc-mongodb f5gc-nrf f5gc-amf f5gc-ausf f5gc-nssf f5gc-pcf  f5gc-smf f5gc-udm f5gc-udr f5gc-webui dbg-rockmongo"
+
+CMD_POOL="apply|delete|show"
+if [[ ! "$1" =~ $CMD_POOL ]]
+then
+    echo "Usage: $0 [ ${CMD_POOL//|/ | } ]"
+    exit 1
+fi
+
+if [ "$1" == "show" ]; then
+    for NF in ${NF_LIST}; do
+        kubectl get deployment -n f5gc ${NF} --no-headers=true
+    done
+else
+    for NF in ${NF_LIST}; do
+        kubectl $1 -k ${NF}
+        sleep 5
+    done
+fi
